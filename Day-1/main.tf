aa343eeaf7e666b5856443a02f820ae466ec9425
Vivek Tiwary <vivekaryannri@gmail.com>
2024-07-12 19:07:43 +0530

    Update main.tf

--- Day-1/main.tf
+++ Day-1/main.tf
@@ -1,10 +1,10 @@
 provider "aws" {
-    region = "us-east-1" 
+  region = "us-east-1"
 }
 
-resource "aws_instance" "example" {
-    ami           = "ami-0c55b159cbfafe1f0" 
+resource "aws_instance" "test2" {
+    ami = "ami-0b72821e2f351e396"
     instance_type = "t2.micro"
-    subnet_id     = ""
-    key_value     = ""
+    subnet_id = "subnet-088acb4f7edaec254"
+    key_name = "ananya"
 }
