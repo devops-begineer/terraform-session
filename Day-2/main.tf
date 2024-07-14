3a9c473f66d2353fa38e40c8df8f73789a82cb3a
Vivek Tiwary <vivekaryannri@gmail.com>
2024-07-14 11:20:59 +0530

    Update main.tf

--- Day-2/main.tf
+++ Day-2/main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "simran" {
     ami = var.ami_value
     instance_type = var.instance_type_value
     subnet_id = var.subnet_id_value
-    key_name = var.key_pair_value
+    key_name = var.key_name_value
 
     tags = {
     Name = "Simran"
