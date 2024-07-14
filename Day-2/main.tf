ae74022c28e2f1529c1c0846a445916bf1f12604
Vivek Tiwary <vivekaryannri@gmail.com>
2024-07-14 10:15:22 +0530

    Update main.tf

--- Day-2/main.tf
+++ Day-2/main.tf
@@ -1,8 +1,12 @@
-resource "aws_instance" "example" {
+resource "aws_instance" "simran" {
     ami = var.ami_value
     instance_type = var.instance_type_value
     subnet_id = var.subnet_id_value
     key_name = var.key_pair_value
+
+    tags = {
+    Name = "Simran"
+    }
 }
 
 resource "aws_s3_bucket" "s3_bucket" {
