9c3688095bfd38cd60bbf71ca4a6635d6f39a8e3
Vivek Tiwary <vivekaryannri@gmail.com>
2024-07-14 11:32:40 +0530

    Update variables.tf

--- Day-4/modules/ec2_instance/variables.tf
+++ Day-4/modules/ec2_instance/variables.tf
@@ -1,1 +1,13 @@
+variable "ami_id" {
+  description = "The AMI to use for the instance"
+}
 
+variable "instance_type" {
+  description = "The type of instance to start"
+  default     = "t2.micro"
+}
+
+variable "instance_name" {
+  description = "The name tag of the instance"
+  default     = "ExampleInstance"
+}
